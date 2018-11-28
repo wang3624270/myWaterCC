5ede76a4c809d4f2e931f5c2328a35987d42c1d2
wang3624270 <3624270@qq.com>
2018-11-28 19:18:05 +0800

    '2018/11/28/1'

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Homepage from '@/components/homepage';
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'homepage',
+      component: Homepage
     }
   ]
 })
